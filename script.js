6c2e62cd9817206272bac07b715959ae5fdd2cfd
Oleg <tysenkov.oleg@gmail.com>
2020-03-31 19:47:47 +0300

    Add While DO

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let money = prompt("Ваш бюджет на месяц?", ''),
+let money = +prompt("Ваш бюджет на месяц?", ''),
     time = prompt("Введите дату в формате YYYY-MM-DD", '');
-    
+
 
 let appData = {
     moneyData: money,
@@ -11,12 +11,71 @@ let appData = {
     savings: false
 };
 
-let quest1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    quest2 = prompt("Во сколько обойдется?", ''),
-    quest3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    quest4 = prompt("Во сколько обойдется?", '');
+for (let i = 0; i < 2; i++) {
+    let quest1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        quest2 = prompt("Во сколько обойдется?", '');
 
-appData.expenses[quest1] = quest2;
-appData.expenses[quest3] = quest4;
+    if ((typeof (quest1)) === 'string' && (typeof (quest1)) != null && (typeof (quest2)) != null && quest1 != '' && quest1.length < 50) {
+        console.log('Done!');
+        appData.expenses[quest1] = quest2;
+    } else {
+        console.log('Breake!');
+        i--;
+    }
 
-alert(appData.moneyData / 30);
+};
+
+
+
+//Цикл WHILE
+
+
+// let i = 0;
+// while (i < 2) {
+//     let quest1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//         quest2 = prompt("Во сколько обойдется?", '');
+
+//     if ((typeof (quest1)) === 'string' && (typeof (quest1)) != null && (typeof (quest2)) != null && quest1 != '' && quest1.length < 50) {
+//         console.log('Done!');
+//         appData.expenses[quest1] = quest2;
+//     } else {
+//         console.log('Breake!');
+//         i--;
+//     }
+//     i++;
+// };
+
+// WHILE DO
+
+// let i = 0;
+
+// do {
+//     let quest1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//         quest2 = prompt("Во сколько обойдется?", '');
+
+
+//     if ((typeof (quest1)) === 'string' && (typeof (quest1)) != null && (typeof (quest2)) != null && quest1 != '' && quest1.length < 50) {
+//         console.log('Done!');
+//         appData.expenses[quest1] = quest2;
+//     } else {
+//         console.log('Breake!');
+//         i--;
+//     }
+//     i++;
+// }
+// while (i < 2);
+
+
+
+appData.moneyPerDay = appData.moneyData / 30;
+alert('Ежедневный бюджет состовляет: ' + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log('Низкий уровень дохода');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('Средний уровень дохода');
+} else if (appData.moneyPerDay > 2000) {
+    console.log('Высокий уровень дохода');
+} else {
+    console.log('Ошибка!');
+};
